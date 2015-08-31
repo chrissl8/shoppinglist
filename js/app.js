9fd2114ea1f1d5c132e7013c1fe19802c0cff702
chrissl8 <chrisjslaight@gmail.com>
2015-08-31 09:23:28 -0400

    UI tweaks and implement remove list item capability

--- js/app.js
+++ js/app.js
@@ -8,10 +8,26 @@ $(document).ready(function(){
 
 		}
 		else {
-		var listHTML = '<li class="red-item">' +  listValue +  '</li>';
+		var listHTML = '<li class="red-item">' +  listValue +  '<br><a class="remove-item-link">X</a></li>';
 		$('.shopping-list').prepend(listHTML); //Add list element
 		$('#list-value').val('');
 		$('#list-value').focus();
 		}
+	})
+
+	$(document).on('click','.remove-item-link', function() {
+		 $(this).closest('li').remove();
+		 $('#list-value').focus();
+		 var listSize = $('.shopping-list li').length;
+		 if(listSize < 1)
+		 {
+		 	//$('.click-to-check').hide();
+			$('.no-items').show();
+		 }
+		 else
+		 {
+			$('.no-items').hide();
+			//$('.click-to-check').show();
+		 }
 	})
 });
