3e406a52a5448b01b5a2a1312bfd60eab17e0b10
chrissl8 <chrisjslaight@gmail.com>
2015-08-31 21:25:13 -0400

    Add favicon, code cleanup and comments, merging of functions in JS file and HTML character escaping for input

--- js/app.js
+++ js/app.js
@@ -1,52 +1,41 @@
+/*
+Where's the Paper Towels?: A Shopping List App
+By Chris Slaight, 2015
+-app.js main JavaScript/jQuery logic for application
+*/
+
+//Main jQuery code to be fired upon page load completion
 $(document).ready(function(){
 	//Functionality to add new items to the list
 	$('#add-item').submit(function(e){
 		e.preventDefault(); //Prevents refresh
-		var listValue = $('#list-value').val(); //Variable to hold input value
+		var rawListValue = $('#list-value').val(); //Variable to hold input value
+		var listValue = escapeHtml(rawListValue);
 		if(listValue == new String(""))
 		{
-			//Just to catch lack of input, form is required so this is a second level of validation
+			//Intentionally blank, just to handle scenarios of blank input
 		}
 		else { //Perform add logic to follow
-		var listHTML = '<li class="red-item">' +  listValue +  '<br><a class="remove-item-link">Remove Item</a></li>'; //Build HTML for new li
-		$('.shopping-list').prepend(listHTML); //Prepend list element to shopping list
-		$('#list-value').val(''); //Clear out the form input
-		$('#list-value').focus(); //Return focus to the input for better usability
+			//Build HTML for new li
+			var listHTML = '<li class="red-item">' +  listValue +  '<br><a class="remove-item-link">Remove Item</a></li>'; 
+			$('.shopping-list').prepend(listHTML); //Prepend list element to shopping list
+			$('#list-value').val(''); //Clear out the form input
+			$('#list-value').focus(); //Return focus to the input for better usability
 		}
 
-		var listSize = $('.shopping-list li').length; //Determine the length of the list for 'no items' text
-		 if(listSize < 1)
-		 {
-		 	//If no list items, show message to add them
-			$('.no-items').show();
-			$('.remove-all-link').hide();
-		 }
-		 else
-		 {
-		 	//If items exist, hide message
-			$('.no-items').hide();
-			$('.remove-all-link').show();
-		 }
+		flipBackgroundMessage(); //Calls method to add/remove 'remove all' button and 'no-items' text
 	})
+
+
 	//Functionality to remove list items
 	$(document).on('click touchstart','.remove-item-link', function() {
 		 $(this).closest('li').remove(); //Choose the closest list item and remove
 		 //$('#list-value').focus(); //Return focus to the input for better usability
 
-		 var listSize = $('.shopping-list li').length; //Determine the length of the list for 'no items' text
-		 if(listSize < 1)
-		 {
-		 	//If no list items, show message to add them
-			$('.no-items').show();
-			$('.remove-all-link').hide();
-		 }
-		 else
-		 {
-		 	//If items exist, hide message
-			$('.no-items').hide();
-			$('.remove-all-link').show();
-		 }
+		 flipBackgroundMessage(); //Calls method to add/remove 'remove all' button and 'no-items' text
 	})
+
+
 	//Functionality to change colors for unchecked/checked
 	$(document).on('click touchstart','.list-items li',function() {
 		
@@ -65,23 +54,42 @@ $(document).ready(function(){
 		
 	})
 
-	$(document).on('click touchstart','.remove-all-link', function() {
-		$('li').remove();
 
-		var listSize = $('.shopping-list li').length; //Determine the length of the list for 'no items' text
+	$(document).on('click touchstart','.remove-all-link', function() {
+		$('li').remove(); //Remove all li elements from the class
+
+		flipBackgroundMessage(); //Calls method to add/remove 'remove all' button and 'no-items' text
+	})
+
+
+	//Function to escape HTML characters from input for added security and hardening
+	function escapeHtml(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ 	}
+
+
+ 	//This function will determine the list of items and either 
+ 	//add/remove the display of the 'no items' and 'remove all' elements
+ 	function flipBackgroundMessage() {
+ 		var listSize = $('.shopping-list li').length; //Determine the length of the list for 'no items' text
 		 if(listSize < 1)
 		 {
-		 	//If no list items, show message to add them
+		 	//If no list items, show message to add them and hide 'remove all' button
 			$('.no-items').show();
 			$('.remove-all-link').hide();
 		 }
 		 else
 		 {
-		 	//If items exist, hide message
+		 	//If items exist, hide message and show 'remove all' button
 			$('.no-items').hide();
 			$('.remove-all-link').show();
 		 }
-	})
+ 	}
 });
 
 
